b9f110840d413bb4d4fc1938c37814ad1588847b
Jip Fr <jipfrijlink@gmail.com>
2022-03-09 22:04:36 +0100

    feat: use Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({}),
 
 		vite: {
 			css: {
